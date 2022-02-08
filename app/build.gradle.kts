04338c9d5db3f9ea451666dce4252a090d587eba
w <wvvwvvvvwvvw@gmail.com>
2022-02-08 04:55:42 -0800

    disable update check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,7 @@ android {
 
     productFlavors {
         create("standard") {
-            buildConfigField("boolean", "INCLUDE_UPDATER", "true")
+            // buildConfigField("boolean", "INCLUDE_UPDATER", "true")
             dimension = "default"
         }
         create("dev") {
